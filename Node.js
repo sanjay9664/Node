ed5a9a99810fb0ebf0c4b75489ed80ab5b2a5893
sanjay gupta <guptaji30749@gmail.com>
2023-05-12 12:48:37 +0530

    product

--- Node.js
+++ Node.js
@@ -1,1 +1,13 @@
-console.log("Hello Word")
+// console.log("Hello Word")
+
+var obj = {
+    name:"sanjay",
+    age:21,
+    class:"10th"
+}
+
+var data = (val)=>{
+  console.log(val.name)
+}
+
+data()

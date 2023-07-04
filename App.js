781d7c4aa21c995ccc3f66c55c11013845d9158c
sanjay gupta <guptaji30749@gmail.com>
2023-07-04 16:43:49 +0530

    spred

--- App.js
+++ App.js
@@ -266,14 +266,41 @@
 
 
 
-const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+// const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
 
-let data = array.map((el)=>{
-    if(el===' '){
-        return "empty string"
-    }else{
-        return el 
-    }
-})
+// let data = array.map((el)=>{
+//     if(el===' '){
+//         return "empty string"
+//     }else{
+//         return el 
+//     }
+// })
 
-console.log(data)
+// console.log(data)
+
+
+
+// 1) 
+// const obj1 = {'key1': 1}
+
+// const obj2 = { ...obj1}
+
+// if(obj2 === obj1){
+
+// console.log('same objects')
+
+// }
+
+// else{
+
+// console.log('different objects')
+
+// }
+
+
+
+// 2)
+const obj1 = {'key1': 1 , 'key2' : 2}
+const obj2 = { ...obj1, key1: 1000}
+console.log(obj1)
+console.log(obj2)

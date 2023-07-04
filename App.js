8d53d574a17ef1f1f8c8438027de550be5cf4a87
sanjay gupta <guptaji30749@gmail.com>
2023-07-04 17:04:25 +0530

    Destructuring

--- App.js
+++ App.js
@@ -280,27 +280,44 @@
 
 
 
+// **********************************************************rest operator *************************************
 // 1) 
 // const obj1 = {'key1': 1}
-
 // const obj2 = { ...obj1}
-
 // if(obj2 === obj1){
+    // console.log('same objects')
+    // }
+    // else{
+        // console.log('different objects')
+        // }
+        
+        
+        
+        // 2)
+        // const obj1 = {'key1': 1 , 'key2' : 2}
+        // const obj2 = { ...obj1, key1: 1000}
+        // console.log(obj1)
+        // console.log(obj2)
 
-// console.log('same objects')
 
-// }
+  // **********************************************************Destructuring  *************************************
 
-// else{
+//   with array 
 
-// console.log('different objects')
+//   const arr = ["sanjay" ,21 ]
+//   const[name, age] = arr
+//   console.log(name)
+//   console.log(age)
 
-// }
+
+// with object 
+
+const person = {
+     name:"sanjay",
+     age: 21
+}
 
 
+const {name , age} = person
 
-// 2)
-const obj1 = {'key1': 1 , 'key2' : 2}
-const obj2 = { ...obj1, key1: 1000}
-console.log(obj1)
-console.log(obj2)
+console.log(name , age)
